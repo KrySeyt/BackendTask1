50266a1e90d53a5036269bda9294714fb194fa66
KrySeyt <kryseyt@gmail.com>
2022-12-28 19:51:42 +0300

    database url optimized

---
+++ app/database.py
@@ -0,0 +1,15 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("BackendTask1HerokuPostgresURI")
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
