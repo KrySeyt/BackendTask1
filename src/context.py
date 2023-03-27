3062bf7f93e97f7e706c703806cbaf3664d6432d
Kryseyt <kryseyt@gmail.com>
2023-03-27 15:33:31 +0300

    small fix

--- src/context.py
+++ src/context.py
@@ -2,4 +2,4 @@ from contextvars import ContextVar
 from uuid import UUID
 
 
-request_uuid: ContextVar[UUID | None] = ContextVar("reqeust_uuid", default=None)
+request_uuid: ContextVar[UUID | None] = ContextVar("request_uuid", default=None)
